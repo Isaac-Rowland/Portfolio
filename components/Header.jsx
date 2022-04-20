2825180fb216069c1eb0280c0185985e3efc8d29
Isaac <ibirowland@gmail.com>
2022-04-20 12:00:47 +1000

    headerNav update

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     <div className="header">
       <Navbar fixed="top" bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Isaac Rowland</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
